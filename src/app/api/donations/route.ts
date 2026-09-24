9d74e1e312e96df8855a07cc0ec25a31d3725093
piyush <piyushjhuria2611@gmail.com>
2026-09-25 01:03:29 +0530

    Add donation API and Supabase integration

---
+++ src/app/api/donations/route.ts
@@ -0,0 +1,102 @@
+import { supabase } from "@/lib/supabase";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+
+    const {
+      title,
+      description,
+      quantity,
+      food_type,
+      expiry_time,
+      latitude,
+      longitude,
+      pickup_address,
+      donor_id,
+    } = body;
+
+    if (!title || !quantity) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: "Food title and quantity are required",
+        },
+        { status: 400 }
+      );
+    }
+
+    const { data, error } = await supabase
+      .from("food_donations")
+      .insert([
+        {
+          title,
+          description,
+          quantity,
+          food_type,
+          expiry_time,
+          latitude,
+          longitude,
+          pickup_address,
+          donor_id,
+        },
+      ])
+      .select()
+      .single();
+
+    if (error) {
+  console.error("SUPABASE DONATION ERROR:", error);
+
+  return NextResponse.json(
+    {
+      success: false,
+      error: error.message,
+    },
+    { status: 500 }
+  );
+}
+
+    return NextResponse.json({
+      success: true,
+      message: "Food donation created successfully!",
+      donation: data,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: "Invalid request",
+      },
+      { status: 400 }
+    );
+  }
+}
+
+
+
+
+
+
+
+export async function GET() {
+  const { data, error } = await supabase
+    .from("food_donations")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json({
+    success: true,
+    donations: data,
+  });
+}
